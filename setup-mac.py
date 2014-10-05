8eca46a134e4b8d5e55ca4522637f83e3e59bfc3
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2014-10-04 21:42:27 -0400

    setup updated.

--- setup-mac.py
+++ setup-mac.py
@@ -5,27 +5,32 @@ import numpy
 import os.path
 
 source_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'src')
+header_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'include')
 
 include_dirs=[r'/Users/kmlee/.anaconda/include',
-              r'/Users/kmlee/code/koretools/include',
+              r'/Users/kmlee/temporary_from_141002/code/koretools/include',
               r'/Users/kmlee/.brew/include/eigen3',
               r'/Users/kmlee/.brew/include',
               numpy.get_include(),
               source_path,
+              header_path,
 ]
 
 library_dirs = [r'/Users/kmlee/.anaconda/lib',
                 r'/Users/kmlee/.brew/lib',
                 ]
-
-extra_compile_args = ['-std=c++11', '-O2', '-DNDEBUG', '-DEIGEN_NO_DEBUG', ]
-
+extra_compile_args = ['-std=c++11', '-O2', ]
+define_macros = [('NDEBUG', 1),
+                 ('EIGEN_NO_DEBUG', 1),
+                 ('USE_TR1', 1),
+             ]
 libraries = ['tbb',]
 
 extensions = [
     Extension('paralinalg',
               ['cython/paralinalg.pyx', ],
               language='c++',
+              define_macros = define_macros,
               extra_compile_args = extra_compile_args,
               libraries = libraries,
               include_dirs = include_dirs,
