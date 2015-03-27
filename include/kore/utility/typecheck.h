c410df1df263f8b847e5ed82c84a833c01133fbe
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2015-03-27 17:10:34 -0400

    typecheck.h changed.

--- include/kore/utility/typecheck.h
+++ include/kore/utility/typecheck.h
@@ -2,10 +2,14 @@
 
 namespace kore {
 
+#ifdef USE_CXX11
+template <typename T, typename U>
+using is_same = std::is_same<T, U>;
+#else
 template <typename A, typename B>
 struct is_same { static const bool value = false; };
-
 template <typename A>
 struct is_same<A, A> { static const bool value = true; };
+#endif
 
 } // namespace kore
