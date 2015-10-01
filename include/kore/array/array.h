62688eecab41fd4dff9d2bc94241f337007afa55
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2015-10-01 15:03:17 -0400

    array_impl.h added.

--- include/kore/array/array.h
+++ include/kore/array/array.h
@@ -15,7 +15,6 @@
 #include "../typedefs.h"
 
 namespace kore {
-
 namespace array {
 
 template <std::size_t Rank> class ArrayStructure;
@@ -192,7 +191,6 @@ private:
 }; // class ArrayStructure
 
 
-
 //! Array<ValueType, Rank>
 template <typename _ValueType, std::size_t _Rank>
 class Array
@@ -324,8 +322,6 @@ public:
   const ArrayStructureType& structure() const { return structure_; }
 
   std::shared_ptr<const ValueType> data() const { return data_; }
-  //PointerType data() { return data_.get(); }
-  //ConstPointerType data() const { return data_.get(); }
   bool own() const { return own_; }
   PointerType begin() { return data_.get(); }
   PointerType end() { return data_.get() + length(); }
@@ -461,7 +457,6 @@ public:
   SizeType shape(size_t d) const { return structure_.shape(d); }
   SizeType stride(size_t d) const { return structure_.stride(d); }
 
-  //friend class Array<ValueType, Rank>;
   friend std::ostream& operator<< <ValueType, Rank>(std::ostream & os, ConstArray<ValueType, Rank> const &);
 private:
   ArrayStructureType structure_;
@@ -469,73 +464,7 @@ private:
   bool own_;
 }; // class ConstArray
 
-
-
-
-
-
-
-
-template <std::size_t Rank>
-std::ostream & operator<<(std::ostream & os, ArrayStructure<Rank> const & as)
-{
-  using SizeType = typename ArrayStructure<Rank>::SizeType;
-  os << "ArrayStructure<" << Rank << ">[";
-  os << "shape :{" << as.shape_[0];
-  for (SizeType d = 1; d < Rank; ++d) {
-    os << ", " << as.shape_[d];
-  }
-  os << "}, ";
-  os << "stride: {" << as.stride_[0];
-  for (SizeType d = 1; d < Rank; ++d) {
-    os << ", " << as.stride_[d];
-  }
-  os << "}, ";
-  os << "length: " << as.length_;
-  os << "]";
-  return os;
-}
-
-template <typename ValueType, std::size_t Rank>
-std::ostream & operator<< (std::ostream& os, Array<ValueType, Rank> const & arr)
-{
-  os << "Array<" << typeid(ValueType).name() << ", " << Rank << ">[";
-  os << arr.structure_;
-  //os << ", data: " << arr.data_;
-  os << ", data(";
-  os << arr.data_.use_count();
-  os << "): {";
-  if (arr.length() > 0) {
-    os << (arr.data_.get())[0];
-  }
-  for (size_t i = 1, n = arr.length(); i < n; ++i) {
-    os << ", " << (arr.data_.get())[i];
-  }
-  os << "}";
-  os << "]";
-  return os;
-}
-
-template <typename ValueType, std::size_t Rank>
-std::ostream & operator<< (std::ostream& os, ConstArray<ValueType, Rank> const & arr)
-{
-  os << "ConstArray<" << typeid(ValueType).name() << ", " << Rank << ">[";
-  os << arr.structure_;
-  //os << ", data: " << arr.data_;
-  os << ", data(";
-  os << arr.data_.use_count();
-  os << "): {";
-  if (arr.length() > 0) {
-    os << (arr.data_.get())[0];
-  }
-  for (size_t i = 1, n = arr.length(); i < n; ++i) {
-    os << ", " << (arr.data_.get())[i];
-  }
-  os << "}";
-  os << "]";
-  return os;
-}
-
 } // namespace array
-
 } // namespace kore
+
+#include "array_impl.h"
---
+++ include/kore/array/array_impl.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "array.h"
+
+namespace kore {
+namespace array {
+template <std::size_t Rank> inline
+std::ostream & operator<<(std::ostream & os, ArrayStructure<Rank> const & as)
+{
+  using SizeType = typename ArrayStructure<Rank>::SizeType;
+  os << "ArrayStructure<" << Rank << ">[";
+  os << "shape :{" << as.shape_[0];
+  for (SizeType d = 1; d < Rank; ++d) {
+    os << ", " << as.shape_[d];
+  }
+  os << "}, ";
+  os << "stride: {" << as.stride_[0];
+  for (SizeType d = 1; d < Rank; ++d) {
+    os << ", " << as.stride_[d];
+  }
+  os << "}, ";
+  os << "length: " << as.length_;
+  os << "]";
+  return os;
+}
+
+template <typename ValueType, std::size_t Rank> inline
+std::ostream & operator<< (std::ostream& os, Array<ValueType, Rank> const & arr)
+{
+  os << "Array<" << typeid(ValueType).name() << ", " << Rank << ">[";
+  os << arr.structure_;
+  os << ", data(";
+  os << arr.data_.use_count();
+  os << "): {";
+  if (arr.length() > 0) {
+    os << (arr.data_.get())[0];
+  }
+  for (size_t i = 1, n = arr.length(); i < n; ++i) {
+    os << ", " << (arr.data_.get())[i];
+  }
+  os << "}";
+  os << "]";
+  return os;
+}
+
+template <typename ValueType, std::size_t Rank> inline
+std::ostream & operator<< (std::ostream& os, ConstArray<ValueType, Rank> const & arr)
+{
+  os << "ConstArray<" << typeid(ValueType).name() << ", " << Rank << ">[";
+  os << arr.structure_;
+  os << ", data(";
+  os << arr.data_.use_count();
+  os << "): {";
+  if (arr.length() > 0) {
+    os << (arr.data_.get())[0];
+  }
+  for (size_t i = 1, n = arr.length(); i < n; ++i) {
+    os << ", " << (arr.data_.get())[i];
+  }
+  os << "}";
+  os << "]";
+  return os;
+}
+
+} // namespace array
+} // namespace kore
